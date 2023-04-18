9093e348bcbb5f2bae4aff191085f5296e4f5f5c
unknown <jamierobertdawson@gmail.com>
2023-04-17 22:18:40 -0700

    completed video 44. Removed anonymous function from render

--- src/App.js
+++ src/App.js
@@ -28,9 +28,19 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }; //A short hand. The key is going to be the name of variable and value is gonna be value of variable
+    });
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state; //destructured. Do this instead of "this.state"
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     console.log("render");
@@ -40,12 +50,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField }; //A short hand. The key is going to be the name of variable and value is gonna be value of variable
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
