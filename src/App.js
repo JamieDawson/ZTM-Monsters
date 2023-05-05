810503faa588687da80beaabf89dfd058351c1af
unknown <jamierobertdawson@gmail.com>
2023-05-05 12:52:50 -0700

    finished app. Moved everything into it's own components

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App extends Component {
     console.log("render from Appjs");
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
