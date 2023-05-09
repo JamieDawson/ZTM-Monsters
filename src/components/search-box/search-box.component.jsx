f9e793e4aa60ccc520b888216b82219533362bb4
unknown <jamierobertdawson@gmail.com>
2023-05-09 15:52:11 -0700

    63. Converted all components to functional components

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,14 @@
-import { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        className={`search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler} //it's a handler
-      />
-    );
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler} //it's a handler
+    />
+  );
+};
 
 export default SearchBox;
